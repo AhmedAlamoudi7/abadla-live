13949272d646ab169149c6c866e8ff65a43202ee
AhmedAlamoudi7 <ahmed.r.alamoudi@gmail.com>
2026-04-27 13:51:20 -0400

    last version

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,14 +38,16 @@ return [
             'report' => false,
         ],
 
-        'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/public/storage',
-            'visibility' => 'public',
+'public' => [
+    'driver' => 'local',
+    'root' => storage_path('app/public'),
+    'url' => env('APP_URL').'/storage/app/public',
+    'visibility' => 'public',
+
             'throw' => false,
             'report' => false,
         ],
+                
                 'livewire' => [
             'driver' => 'local',
             'root' => storage_path('app'),
