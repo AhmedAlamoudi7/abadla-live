18e66864caede4fb2daa2f1f605d2004212aac09
Ahmed Alamoudi <ahmed.r.alamoudi@gmail.com>
2026-05-20 14:16:49 +0300

    fix issues and add features

--- cms/resources/views/site/partials/breaking.blade.php
+++ cms/resources/views/site/partials/breaking.blade.php
@@ -1,8 +1,10 @@
-<div class="breaking container" data-animate="fade-up">
-    <span class="badge">آخر الأخبار</span>
-    <div class="breaking-wrapper">
-        <div class="breaking-track">
-            {{ $site['breaking_ticker'] ?? '' }}
+<div class="container">
+    <div class="breaking" data-animate="fade-up">
+        <span class="badge">آخر الأخبار</span>
+        <div class="breaking-wrapper">
+            <div class="breaking-track">
+                {{ $site['breaking_ticker'] ?? '' }}
+            </div>
         </div>
     </div>
 </div>
